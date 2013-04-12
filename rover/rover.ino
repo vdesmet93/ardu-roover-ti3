2b08d07148af36bdc866989ab6960890df5735c2
Remyoman <remyoman@gmail.com>
2013-04-12 12:39:34 +0200

    Defined magic cookies and cleanup.

--- rover/rover.ino
+++ rover/rover.ino
@@ -2,6 +2,7 @@
 #include <SoftwareSerial.h>
 #include "commands.h"
 
+#define DEBUG
 #define RX 10
 #define TX 11
 #define JOYSTICK_RIGHT_X  A0
@@ -9,19 +10,30 @@
 #define JOYSTICK_LEFT_Y  A2
 #define JOYSTICK_LEFT_X  A3
 #define SONAR_FREQUENCY  A3
-#define EASTER_EGG  A5
+#define DIRECTION  A5
 #define KILL_SWITCH  4
 #define MANUAL_AUTO  2
-
+#define SYNC_DELAY  100
+#define LOOP_DELAY  50
+#define KILL_DELAY  10
+#define MAX_SPEED  35000
+#define REVERSE_THRESHOLD  2000
+#define SPEED_THRESHOLD  1100
+#define SPEED_COMPENSATOR  50
+#define HALT_THRESHOLD  1100
+#define ANGLE_THRESHOLD  1100
+#define KILLSWITCH_THRESHOLD  1100
 #define bit9600Delay 84  
+#define BAUD_RATE  9600
 
 SoftwareSerial mySerial(RX, TX); // RX, TX
-int number,n, fd;
+int number, n, fd;
+int current = 0;
 
 void readFromRover() 
 {
-  while(mySerial.available() > 0) {
-
+  while(mySerial.available() > 0) 
+  {
     int i = mySerial.read();
     char str[256];
     sprintf(str, "%x ", i);
@@ -51,34 +63,11 @@ int getChecksum(unsigned char* buf)
   return c;
 }
 
-int writeSerial(unsigned char* buf, int length) {
-  for(int i = 0; i < length; i++)  
-  {
-    mySerial.write(buf[i]);
-    // SWprint(buf[i]);
-  }
-
-  return length;
-}
-
-void SWprint(int data)
+int writeSerial(unsigned char* buf, int length) 
 {
-  byte mask;
-  //startbit
-  digitalWrite(TX,LOW);
-  delayMicroseconds(bit9600Delay);
-  for (mask = 0x01; mask>0; mask <<= 1) {
-    if (data & mask){ // choose bit
-      digitalWrite(TX,HIGH); // send 1
-    }
-    else{
-      digitalWrite(TX,LOW); // send 0
-    }
-    delayMicroseconds(bit9600Delay);
-  }
-  //stop bit
-  digitalWrite(TX, HIGH);
-  delayMicroseconds(bit9600Delay);
+  for(int i = 0; i < length; i++)  
+    mySerial.write(buf[i]);
+  return length;
 }
 
 /**
@@ -106,10 +95,6 @@ void sendPacket(char command)
   buf[5] = low;
   // send the array, print resultcode
   int resultcode = writeSerial (buf, 6);
-
-  //char str[256];
-  //sprintf(str, "Resultcode: %d", resultcode);
- // Serial.println(str);
 }
 
 /*
@@ -117,86 +102,68 @@ void sendPacket(char command)
  */
 void sendPacket(char command, int argument) 
 {
-  // create array. Length = 9: 2 header, 1 byte count, 1 command, 1 arg. type, 2 argument, 
-  //														2 checksum
+  //create array. Length = 9: 2 header, 1 byte count, 1 command, 1 arg. type, 2 argument, 
+  //2 checksum
   unsigned char buf[9];
   /* ================= HEADER ================= */
   buf[0] = 0xFA; // 250
   buf[1] = 0xFB; // 251
-
   /* =============== PACKET LENGTH =============== */
   // command + 2 byte checksum + 2 byte message
   buf[2] = 6; 
-
   /* ================= COMMAND ================= */
   buf[3] = command;
-
   /* ============== ARGUMENT TYPE ============== */
   if(argument >= 0)
     buf[4] = (unsigned char) ARG_POS_INT;
-  else {
+  else 
+  {
     argument *= -1;
     buf[4] = (unsigned char) ARG_NEG_INT;
   }
-
   /* ================= ARGUMENT ================= */
   // parse argument high and low
   char highArgument = ( (argument & 0xFF00) >> 8);
   char lowArgument = (argument & 0x00FF);
-  
-
   // add argument to array. 
   // Low byte first(instead of high-first for checksum, because f*ck consistency)
   buf[5] = lowArgument;
   buf[6] = highArgument;
-
   /* ================= CHECKSUM ================= */
   // generate checksum
   int checksum = getChecksum(buf);
-
   // parse the checksum
   char highChecksum = (checksum & 0xFF00) >> 8;
   char lowChecksum = checksum & 0x00FF;
-
   // add checksum to array, high byte is first
   buf[7] = highChecksum;
   buf[8] = lowChecksum;
-
   /* ================= TRANSFER ================= */
   // send the array, print resultcode
   int resultcode = writeSerial (buf, 9);
-
-  //char str[256];
- // sprintf(str, "Resultcode: %d", resultcode);
-  //Serial.println(str);
 }
+
 /*
 * Send a packet with a string as argument
  */
-void sendPacket(char command, char* argument, int size) {
+void sendPacket(char command, char* argument, int size) 
+{
   // create array. Length = 9: 2 header, 1 byte count, 1 command, 1 arg. type, 2 argument, 
-  //														2 checksum
+  //2 checksum
   int bytes = size + 7;
-
   unsigned char buf[bytes];
-
   /* ================= HEADER ================= */
   buf[0] = 0xFA; // 250
   buf[1] = 0xFB; // 251
-
   /* =============== PACKET LENGTH =============== */
   // command + 2 byte checksum + 2 byte message
   buf[2] = 6; 
-
   /* ================= COMMAND ================= */
   buf[3] = command;
-
   /* ============== ARGUMENT TYPE ============== */
   buf[4] = (unsigned char) ARG_STRING;
-
   /* ================= ARGUMENT ================= */
   buf[5] = size;
-
   // parse argument high and low
   printf("parsing\n");
   char c = *argument;
@@ -209,28 +176,22 @@ void sendPacket(char command, char* argument, int size) {
     idx++;
     c = *argument;
   }
-
   /* ================= CHECKSUM ================= */
   // generate checksum
   int checksum = getChecksum(buf);
-
   // parse the checksum
   char highChecksum = (checksum & 0xFF00) >> 8;
   char lowChecksum = checksum & 0x00FF;
-
   // add checksum to array, high byte is first
   buf[bytes-2] = highChecksum;
   buf[bytes-1] = lowChecksum;
-
   /* ================= TRANSFER ================= */
   // send the array, print resultcode
   int resultcode = writeSerial (buf, bytes);
-//  printf("Resultcode: %d\n", resultcode);
 }
 
 void setup() 
 {
-  // define pin modes for tx, rx:
   pinMode(RX,INPUT);
   pinMode(TX, OUTPUT);
   pinMode(JOYSTICK_RIGHT_X, INPUT);
@@ -238,137 +199,74 @@ void setup()
   pinMode(JOYSTICK_LEFT_X, INPUT);
   pinMode(JOYSTICK_LEFT_X, INPUT);
   pinMode(SONAR_FREQUENCY, INPUT);
-  pinMode(EASTER_EGG, INPUT);
+  pinMode(DIRECTION, INPUT);
   pinMode(KILL_SWITCH, INPUT);
   pinMode(MANUAL_AUTO, INPUT);
   digitalWrite(TX, HIGH);
   digitalWrite(RX, LOW);
-  
-  Serial.begin(9600);
-
-  mySerial.begin(9600);
-  delay(100);
-
-  //pinMode(9, INPUT);
+  Serial.begin(BAUD_RATE);
+  mySerial.begin(BAUD_RATE);
+  delay(SYNC_DELAY);
   sendPacket(SYNC0);
-  delay(100);  
-
+  delay(SYNC_DELAY);  
   sendPacket(SYNC1);
-  delay(100);
-  //readFromRover();
+  delay(SYNC_DELAY);
   sendPacket(SYNC2);
-  delay(100);
-
+  delay(SYNC_DELAY);
   sendPacket(OPEN);
-  delay(100);
-
-
- // sendPacket(SONAR, 0);
-   sendPacket(BUMPSTALL, 0);
-  delay(100);
-
-  // enable the motors	
+  delay(SYNC_DELAY);
+  sendPacket(SONAR, 0);
+  delay(SYNC_DELAY);
+  sendPacket(BUMPSTALL, 0);
+  delay(SYNC_DELAY);
   sendPacket(ENABLE, 1);
-  delay(100);    
+  delay(SYNC_DELAY);    
 }
-
-int current = 0;
 
 void loop()
 {
-    sendPacket(PULSE);
-
-    // 1500 avg
-    // 1100 min
-    // 1900 max
-    int x = pulseIn(JOYSTICK_RIGHT_X, HIGH);
-    if(x > 1100) {
-     // 0-100
-     int percentage = (x - 1100) / 8;
-     percentage = 100 - percentage;
-     percentage -= 50; 
-     sendPacket(ROTATE, percentage);
-     delay(50);
-    }
-    
-    
-    int easteregg = pulseIn(EASTER_EGG, HIGH);
-    Serial.println(easteregg);
-    
-    boolean backward = false;
-    boolean egg = false;
-    
-    if(easteregg < 1000) {
-      // normal setup
-    }
-    else if(easteregg > 1000 && easteregg < 2000) {
-      egg = true;
-    }
-    else if(easteregg > 2000) {
-      backward = true;
-    }
-    
-    unsigned int s = pulseIn(JOYSTICK_LEFT_Y, HIGH);
-    
-//     Serial.print("S: ");
- //   Serial.println(s);
-    
-    if(s > 1100) {
-      s = s - 1120;
-      if(s > 0 && s < 35000) {
-       s *= 4;
-       
-       if(egg)
-         s = 0;
-       
-       if(backward)
-         sendPacket(VEL, -s);      
-       else
-          sendPacket(VEL, s);      
-  //     Serial.println(s);
-      }
-      else {
-        sendPacket(VEL, 0);
-    //     Serial.println("Stop");   
-      }
-    }
-    else {
-      sendPacket(PULSE);
-    }
-
-    int killswitch = pulseIn(KILL_SWITCH, HIGH);
-    //Serial.print("Kill: ");
-   // Serial.println(killswitch);
-    
-    if(killswitch > 1100) {
-      sendPacket(E_STOP);
-      delay(10);
-    }    
-   
-    delay(50);
-    
-    
-   
-  
-  /**
-  // rotate
-  sendPacket(DHEAD, 180);
-  delay(1000);
   sendPacket(PULSE);
-  delay(1000);
-  sendPacket(PULSE);
-  delay(1000);
-
-  // start driving
-  sendPacket(VEL, 10000);
-  delay(1000);
-  while(1) 
+  int anglePulse = pulseIn(JOYSTICK_RIGHT_X, HIGH);
+  if(anglePulse > ANGLE_THRESHOLD)
   {
-    readFromRover();
-
+    int angle = 100 - ((anglePulse - ANGLE_THRESHOLD) / 8);
+    angle -= 50; 
+    sendPacket(ROTATE, angle);
+  }
+  int directionPulse = pulseIn(DIRECTION, HIGH);
+  boolean backward = false;
+  boolean halt = false;
+  if(directionPulse > HALT_THRESHOLD && directionPulse < REVERSE_THRESHOLD) 
+    halt = true;
+  else if(directionPulse > REVERSE_THRESHOLD) 
+    backward = true;
+  unsigned int spd = pulseIn(JOYSTICK_LEFT_Y, HIGH);
+  if(spd > SPEED_THRESHOLD)
+  {
+    spd = spd - (SPEED_THRESHOLD + SPEED_COMPENSATOR);
+    if(spd > 0 && spd < MAX_SPEED) 
+    {
+      spd *= 4;
+      if(halt)
+        spd = 0;
+      if(backward)
+        sendPacket(VEL, -spd);      
+      else
+        sendPacket(VEL, spd);
+    }
+    else 
+      sendPacket(VEL, 0); 
+  }
+  else
     sendPacket(PULSE);
-    delay(100);
-  }**/
+  int killPulse = pulseIn(KILL_SWITCH, HIGH);
+  if(killPulse > KILLSWITCH_THRESHOLD) 
+  {
+    sendPacket(E_STOP);
+    delay(KILL_DELAY);
+  }    
+  delay(LOOP_DELAY);
 }
+
 
 
